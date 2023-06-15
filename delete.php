d2fd59f29200f67bb43491012af101651ddbd685
Gurpreet Singh <gsinghkaur1012@gmail.com>
2023-06-15 14:56:24 +0200

    1

--- delete.php
+++ delete.php
@@ -16,10 +16,9 @@ $stmt->execute($params);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../hotel_ter_duin/style/reset.css">
-    <link rel="stylesheet" href="../hotel_ter_duin/style/reservering.css">
-    <script src="https://kit.fontawesome.com/a46b3f773e.js" crossorigin="anonymous" defer></script>
-    <title>Hotel Ter Duin - Reserveren</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
+    <title>Document</title>
 </head>
 <body>
     <main>
